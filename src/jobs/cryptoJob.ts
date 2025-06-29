794bd7e508f27d042d302f4ccf8f52a74ad37747
Abhinav Maurya <abhinavmaurya605@gmail.com>
2025-06-29 22:12:03 +0530

    fix: update cron job schedule to run every 5 minutes for crypto data fetching

--- src/jobs/cryptoJob.ts
+++ src/jobs/cryptoJob.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { fetchAndStoreCryptoData } from "../services/crypto.service";
 
 export function startCryptoCronJob() {
-  cron.schedule("0 * * * *", async () => {
+  cron.schedule("*/5 * * * *", async () => {
     console.log("Running crypto data fetch job...");
     await fetchAndStoreCryptoData();
   });
